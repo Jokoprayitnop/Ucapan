17b4c923f2e8ee7a4057d28cd19edb9521062c74
Jokoprayitnop <114488659+Jokoprayitnop@users.noreply.github.com>
2023-01-03 21:28:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,3 @@ onload = () =>{
         document.body.classList.remove("container");
     }, 0);
 };
-
-window.addEventListener('load', () => {
-  Swal.fire({
-    title: 'Ingin Memutar Musik di Latar Belakang?',
-    // text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Ya',
-    cancelButtonText: 'Tidak',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      document.querySelector('.song').play();
-      resolveFetch().then(animationTimeline());
-    } else {
-      resolveFetch().then(animationTimeline());
-    }
-  });
-});
