7a7e4a7374344f71b9630d0cb05809b4fe9546ee
Jokoprayitnop <114488659+Jokoprayitnop@users.noreply.github.com>
2023-01-14 22:27:45 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+$(".deleteRecord").click(function(){
+		//alert("test");
+		var id= $(this).attr('rel');
+		//alert(id);
+		var deletefunction= $(this).attr('rel1');
+		//alert(deletefunction);
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You will not be able to recover this record again!",
+			type:"warning",
+			showCancelButton: true,
+			confirmButtonClass: "btn-danger",
+			confirmButtonText: "Yes, delete it!"
+		},
+		function(){
+			window.location.href="/admin/"+deleteFunction+"/"+id;
+		});
+
 $(document).ready(function () {
   // Render Background Random Shape
   var html = "";
